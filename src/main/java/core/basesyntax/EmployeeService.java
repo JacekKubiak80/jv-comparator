e0c5f1854dee69c07dd36f3045f3d1a127f23df7
Jacek Kubiak <jacek.ku@poczta.fm>
2025-04-19 08:20:31 +0200

    task to check

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,23 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+
+        Comparator<Employee> employeeComparator = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                int ageCompares = o1.getAge().compareTo(o2.getAge());
+                if (ageCompares !=0) {
+                    return ageCompares;
+                }
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+        };
+        return Set.of();
     }
 }
