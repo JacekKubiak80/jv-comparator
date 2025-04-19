9b932c64c22f79843e69d22a65c01c28d1a4891b
Jacek Kubiak <jacek.ku@poczta.fm>
2025-04-19 08:55:31 +0200

    task to check again 1

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -11,15 +11,17 @@ public class EmployeeService {
         Comparator<Employee> employeeComparator = new Comparator<Employee>() {
             @Override
             public int compare(Employee o1, Employee o2) {
-                int ageCompares = o1.getAge().compareTo(o2.getAge());
-                if (ageCompares !=0) {
+                int ageCompares = o1.getName().compareTo(o2.getName());
+                if (ageCompares != 0) {
                     return ageCompares;
                 }
-                return String.compare(o1.getName(), o2.getName());
+                return Integer.compare(o1.getAge(), o2.getAge());
             }
         };
-        Set<Employee> employee = new TreeSet<>(employeeComparator);
 
-        return employee;
+        Set<Employee> employeeSet = new TreeSet<>(employeeComparator);
+        employeeSet.addAll(employees);
+
+        return employeeSet;
     }
 }
